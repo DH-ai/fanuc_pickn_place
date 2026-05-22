efcd4790195735dccdeb39517fa52971513e748f
Dhruv Chaturvedi <dhruvastro67@gmail.com>
2026-05-22 15:33:36 +0530

    added build script

---
+++ fanuc_build_script.bash
@@ -0,0 +1,18 @@
+echo Installing and configuring git-lfs
+sudo apt install git-lfs
+git lfs install
+
+echo Checking out GitHub repositories
+mkdir ~/ws_fanuc/src -p
+cd ~/ws_fanuc/src
+git clone https://github.com/FANUC-CORPORATION/fanuc_description.git
+git clone --branch main --single-branch --recurse-submodules https://github.com/FANUC-CORPORATION/fanuc_driver.git
+
+echo Installing FANUC dependencies
+cd ~/ws_fanuc
+sudo apt update
+rosdep update
+rosdep install --ignore-src --from-paths src -y
+
+echo Building FANUC libraries
+colcon build --symlink-install --cmake-args -DBUILD_TESTING=1 -DBUILD_EXAMPLES=1
